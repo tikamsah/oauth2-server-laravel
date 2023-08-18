135faa2f4fc56ea3792e479d22fbb5cb4e45e777
Tikam <tikamshah@gmail.com>
2023-08-18 15:56:00 +0530

    Update FluentAccessToken.php

--- src/Storage/FluentAccessToken.php
+++ src/Storage/FluentAccessToken.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace LucaDegasperi\OAuth2Server\Storage;
+namespace Tikamsah\OAuth2Server\Storage;
 
 use Carbon\Carbon;
 use League\OAuth2\Server\Entity\AccessTokenEntity;
