73fce48497b4218ccec8cd27939cf3a0ca8b8398
Tikam <tikamshah@gmail.com>
2023-08-18 15:57:34 +0530

    Update FluentScope.php

--- src/Storage/FluentScope.php
+++ src/Storage/FluentScope.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace LucaDegasperi\OAuth2Server\Storage;
+namespace Tikamsah\OAuth2Server\Storage;
 
 use Illuminate\Database\ConnectionResolverInterface as Resolver;
 use League\OAuth2\Server\Entity\ScopeEntity;
