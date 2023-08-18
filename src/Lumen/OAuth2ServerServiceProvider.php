5af92b57ab787b3737a4974630dd2b3670743eb7
Tikam <tikamshah@gmail.com>
2023-08-18 13:55:44 +0530

    Update OAuth2ServerServiceProvider.php

--- src/Lumen/OAuth2ServerServiceProvider.php
+++ src/Lumen/OAuth2ServerServiceProvider.php
@@ -3,22 +3,22 @@
 /*
  * This file is part of OAuth 2.0 Laravel.
  *
- * (c) Luca Degasperi <packages@lucadegasperi.com>
+ * (c) Luca Degasperi <packages@Tikamsah.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace LucaDegasperi\OAuth2Server\Lumen;
+namespace Tikamsah\OAuth2Server\Lumen;
 
-use LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider as ServiceProvider;
+use Tikamsah\OAuth2Server\OAuth2ServerServiceProvider as ServiceProvider;
 
 /**
  * This is the Lumen oauth server service provider class.
  *
  * @deprecated since version 5.1. Use the base OAuth2ServerServiceProvider instead.
  *
- * @author Luca Degasperi <packages@lucadegasperi.com>
+ * @author Luca Degasperi <packages@Tikamsah.com>
  */
 class OAuth2ServerServiceProvider extends ServiceProvider
 {
